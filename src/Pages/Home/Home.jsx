57313bf5f2bdfc7b2434bec162cf5ba6a5dec991
Melanyg24 <meelanyg2003@gmail.com>
2024-06-08 15:17:57 -0500

    actualizacion

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -5,7 +5,7 @@ const Home = () => {
   return (
     <div className=" flex justify-center items-center bg-[url('/bg-main.jpg')] bg-cover bg-center h-screen flex-col">
       <div className='flex flex-col justify-center items-center'>
-        <h1 className=" text-3xl md:text-7xl 2xl:text-8xl xl:text-5xl md:tracking-wide text-blue-900 font-bold pt-20">AMARA <span className="text-[#D31010]">LA MUÑECA POSEIDA</span></h1>
+        <h1 className=" text-3xl md:text-7xl 2xl:text-8xl xl:text-5xl md:tracking-wide text-white font-bold pt-20">AMARA <span className="text-[#D31010]">LA MUÑECA POSEIDA</span></h1>
         <p className="text-[#D31010] text-lg md:text-5xl 2xl:text-6xl xl:text-3xl pt-5">MAPA TRANSMEDIA</p>
       </div>
       <div className="p-8 mt-5 mx-4 rounded-2xl bg-[#00000080] backdrop-blur-sm">
