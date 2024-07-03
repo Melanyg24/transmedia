60662cde2540be3f7d341f50dae8426e9d0cc697
johalama2023 <johalama2023@gmail.com>
2024-07-03 11:26:31 -0500

    actualizando

--- src/Pages/Timeline/TimeLine.jsx
+++ src/Pages/Timeline/TimeLine.jsx
@@ -1,19 +1,58 @@
 import React from "react";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import { motion } from "framer-motion";
+
+import "react-vertical-timeline-component/style.min.css";
+
+import { styles } from "../../styles";
+import { experiences } from "../../constants";
+import { SectionWrapper } from "../../hoc";
+import { textVariant } from "../../utils/motion";
+
+const ExperienceCard = ({ exp }) => (
+  <VerticalTimelineElement
+    contentStyle={{
+      background: "#D31010",
+      color: "#fff",
+    }}
+    contentArrowStyle={{ borderRight: "10px solid  #D31010" }}
+    date={<span style={{ fontSize: "30px", color:"#fff", marginTop:"20px" }}>{exp.date}</span>}
+    icon={
+      <div className="flex justify-center items-center w-full h-full bg-white rounded-full">
+        <img
+          src={exp.icon}
+          alt={exp.company_name}
+          className="w-[100%] h-[100%] object-contain"
+        />
+      </div>
+    }
+  >
+    <div>
+      <img className="w-[40%] mx-auto" src={exp.img} alt="" />
+      <h3 className="text-center text-white text-[40px] font-bold text-shadow-black">" {exp.title} "</h3>
+      <h3 className="text-white text-shadow-black text-center text-[30px]">{exp.company_name}</h3>
+      <h3 className="text-[25px] text-shadow-black text-center">{exp.points}</h3>
+    </div>
+  </VerticalTimelineElement>
+);
 
 const TimeLine = () => {
   return (
     <div className="flex flex-col justify-center items-center min-h-screen bg-[url('/bg-main.jpg')] bg-cover bg-center bg-fixed">
-      <div className="flex flex-col justify-center items-center">
-        <h1 className="text-3xl md:text-7xl xl:text-6xl 2xl:text-8xl md:tracking-wide text-[#D31010] text-shadow-white text-center font-bold pt-20">
-            Linea de Tiempo
+      <div className="flex flex-col justify-center items-center mt-40 xl:mt-52 2xl:mt-80">
+        <h1 className="text-5xl md:text-7xl xl:text-8xl 2xl:text-8xl md:tracking-wide text-[#D31010] text-shadow-white text-center font-bold pt-20">
+          Linea de Tiempo
         </h1>
       </div>
-      <div className="p-8 mt-5 mx-4 rounded-2xl bg-[#00000080] backdrop-blur-sm">
-        <img
-          src="./TimeLine.png"
-          alt="map"
-          className="w-full h-full lg:w-[750px] xl:w-[900px] xl:h-[350px] 2xl:w-[750px]"
-        />
+      <div className="mt-20 flex flex-col w-full xl:w-full">
+        <VerticalTimeline>
+          {experiences.map((exp, index) => (
+            <ExperienceCard key={index} exp={exp} />
+          ))}
+        </VerticalTimeline>
       </div>
     </div>
   );
---
+++ src/constants/index.js
@@ -0,0 +1,74 @@
+import {
+  mobile,
+  backend,
+  creator,
+  web,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  mongodb,
+  git,
+  figma,
+  docker,
+  meta,
+  starbucks,
+  tesla,
+  shopify,
+  carrent,
+  jobit,
+  tripguide,
+  threejs,
+  iconoGrito,
+} from "../assets";
+
+
+
+
+const experiences = [
+  {
+    title: "El Origen",
+    company_name: "Pelicula",
+    icon: iconoGrito,
+    iconBg: "#383E56",
+    date: "2024",
+    points: "La historia comienza con unos adolecentes que juegan a la ouija y abren un portal",
+    img: "/portada-pelicula.jpg"
+  },
+  {
+    title: "Web Serie",
+    company_name: "",
+    icon: iconoGrito,
+    iconBg: "#383E56",
+    date: "2025",
+    points: "Aqui se desarrolla la historia principal donde se explicara como la muñeca manipulará a la actriz principal",
+    img: ""
+  },
+  {
+    title: "La Busqueda",
+    company_name: "Libro",
+    icon: iconoGrito,
+    iconBg: "#383E56",
+    date: "2025",
+    points: "Mia Rodriguez se va a la casa de sus padres adoptivos",
+    img: "/portada-libro.jpg"
+  },
+  {
+    title: "El Despertar",
+    company_name: "Video Juego",
+    icon: iconoGrito,
+    iconBg: "#383E56",
+    date: "2026",
+    points: "El Usuario tomara el control de las hermanas para escapar de la ciudad y esconderse de la policia",
+    img: "/portada-juego.jpg"
+  },
+
+];
+
+
+
+export { experiences };
